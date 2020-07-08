2af035343435961a4bce1b5c757b49af2362be59
Lorenzo Hiver <50888979+LorenzoHiver@users.noreply.github.com>
2020-07-09 01:21:14 +0200

    Delete test.js

--- api/test.js
+++
@@ -1,9 +0,0 @@
-const names = ["Lorenzo","Charlotte","Dylan","Giovanni","Maelia"];
-
-// const exemple = names.map((name) => {
-//     console.log(`Hello, je m'appelle ${name} !`);
-// });
-
-for (const name of names) {
-    console.log(`Hello, je m'appelle ${name} !`);
-}
